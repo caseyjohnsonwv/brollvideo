624d2f970b83c6dcfbfdcc8c69aa7b706c8688fb
Casey Johnson <ctj0001@mix.wvu.edu>
2019-09-22 13:48:59 -0400

    4 beats of black screen generate before/after video

--- broll.sh
+++ broll.sh
@@ -1,6 +1,6 @@
 #check inputs
 if [ $# -lt 3 ]; then
-  printf "Usage: broll.sh <input_directory> <output_file> <audio_tempo> [<audio_file>]\n"
+  printf "Usage: broll.sh <input_directory> <output_file> <tempo> [<audio_file>]\n"
   exit
 fi
 
@@ -19,10 +19,12 @@ fi
 
 #build input and filtergraph for ffmpeg command
 printf "Selecting start/end times for each clip...\n"
-CLIPNUM=0
+CLIPNUM=1
 FILTERGRAPH_CUT=""
 FILTERGRAPH_CONCAT=""
 CLIPLIST=""
+DIMENSIONS="1920x1080"
+BLACKSCREENFILE="videoblackscreens.mp4"
 for CLIPNAME in $(ls -1 "$CLIPDIR" | shuf); do
   BEATS=$((2*$(shuf -i 1-4 -n 1)))
   CLIPTIME=$(awk "BEGIN {print ($BEATS*60/$TEMPO)}")
@@ -34,12 +36,22 @@ for CLIPNAME in $(ls -1 "$CLIPDIR" | shuf); do
   FILTERGRAPH_CONCAT="$FILTERGRAPH_CONCAT[v$CLIPNUM][a$CLIPNUM]"
   CLIPNUM=$(($CLIPNUM+1))
 done
-FILTERGRAPH="$FILTERGRAPH_CUT""$FILTERGRAPH_CONCAT""concat=n=$TOTALCLIPS:v=1:a=1[outv][outa] -map [outv] -map [outa]"
 
+#add black screens to beginning/end
+FOURBEATS=$(awk "BEGIN {print (240/$TEMPO)}")
+ffmpeg -f lavfi -i color=c=black:s=$DIMENSIONS:d=$FOURBEATS -f lavfi -i aevalsrc=0:d=$FOURBEATS $BLACKSCREENFILE -y -hide_banner
+CLIPLIST="-i $BLACKSCREENFILE $CLIPLIST -i $BLACKSCREENFILE"
+FILTERGRAPH_CUT="[0:v]null[v0];[0:a]anull[a0];$FILTERGRAPH_CUT[$(($TOTALCLIPS+1)):v]null[v$(($TOTALCLIPS+1))];[$(($TOTALCLIPS+1)):a]anull[a$((TOTALCLIPS+1))];"
+FILTERGRAPH_CONCAT="[v0][a0]$FILTERGRAPH_CONCAT[v$(($TOTALCLIPS+1))][a$(($TOTALCLIPS+1))]"
+FILTERGRAPH="$FILTERGRAPH_CUT""$FILTERGRAPH_CONCAT""concat=n=$(($TOTALCLIPS+2)):v=1:a=1[outv][outa] -map [outv] -map [outa]"
+
+#do the thing
 if [ -f $AUDIOFILE ]; then
-  ffmpeg $CLIPLIST -filter_complex $FILTERGRAPH ."$OUTPUTFILE" -y
-  ffmpeg -i ."$OUTPUTFILE" -i "$AUDIOFILE" -safe 0 -c copy -map 0:v:0 -map 1:a:0 -shortest "$OUTPUTFILE" -y
+  ffmpeg $CLIPLIST -filter_complex $FILTERGRAPH ."$OUTPUTFILE" -y -hide_banner
+  ffmpeg -i ."$OUTPUTFILE" -i "$AUDIOFILE" -safe 0 -c copy -map 0:v:0 -map 1:a:0 -shortest "$OUTPUTFILE" -y -hide_banner
   rm ."$OUTPUTFILE" 2>/dev/null
 else
   ffmpeg $CLIPLIST -filter_complex $FILTERGRAPH "$OUTPUTFILE" -y
 fi
+
+rm $BLACKSCREENFILE
